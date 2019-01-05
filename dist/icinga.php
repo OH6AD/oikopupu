4d27be662fb6aab32a54ca409e6ab22f1822880f
Joel Lehtonen <joel.lehtonen@iki.fi>
2019-01-05 05:14:37 +0200

    Add simple network status screen

---
+++ dist/icinga.php
@@ -0,0 +1,40 @@
+<?php
+
+require_once __DIR__ . '/../common.php';
+
+$ch = curl_init();
+curl_setopt_array($ch, [
+    CURLOPT_URL => 'https://net.pupu.li/icingaweb2/monitoring/list/hosts?format=json',
+    CURLOPT_NETRC => TRUE,
+    CURLOPT_RETURNTRANSFER => TRUE,
+    CURLOPT_FAILONERROR => TRUE,
+    //CURLOPT_VERBOSE => TRUE,
+    CURLOPT_HTTPHEADER => [
+        "Accept: application/json"
+    ],
+]);
+$json = curl_exec($ch);
+// FIXME process errors
+
+$items = json_decode($json);
+
+$space = '${space}';
+
+echo <<<EOF
+#!ipxe
+set space:hex 20:20
+set space \${space:string}
+menu Tilannehuone
+item Palaa päävalikkoon
+
+EOF;
+
+foreach ($items as $item) {
+    print("item {$item->host_name} {$item->host_display_name}\n");
+    print("item --gap $space $item->host_output\n");
+}
+
+echo <<<EOF
+choose --default exit tgt
+
+EOF;
